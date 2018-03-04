cd833475ad68770a25f7d7f53479d806688389bd
wei_lin_chang <a040506@wei-lin-changs-macbook-pro.local>
2018-03-04 14:09:50 +0800

    created GPACalculatorViewController

---
+++ GPA Calculator/GPACalculatorViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GPACalculatorViewController.swift
+//  GPA Calculator
+//
+//  Created by wei_lin_chang on 04/03/2018.
+//  Copyright © 2018 National Taiwan University. All rights reserved.
+//
+
+import UIKit
+
+class GPACalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
