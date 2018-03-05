3a4a10342ecaea9ac51611c06c16453f98ef1a12
wei_lin_chang <a040506@wei-lin-changs-macbook-pro.local>
2018-03-05 11:34:40 +0800

    some VC setup for collectionView

--- GPA Calculator/GPACalculatorViewController.swift
+++ GPA Calculator/GPACalculatorViewController.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
-class GPACalculatorViewController: UIViewController {
-
+class GPACalculatorViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // MARK: - IB stuff
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var subjectsCollectionView: UICollectionView! {
+        didSet {
+            subjectsCollectionView.dataSource = self
+            subjectsCollectionView.delegate = self
+        }
+    }
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    // MARK: - model
+    var myCalculator = GPACalculator()
+    
+    // MARK: - collectionViewDataSource/Delegate funcs
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return myCalculator.subjects.count
+        default: return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    // MARK: - VC lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ GPA Calculator/sujectCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  sujectCollectionViewCell.swift
+//  GPA Calculator
+//
+//  Created by wei_lin_chang on 05/03/2018.
+//  Copyright © 2018 National Taiwan University. All rights reserved.
+//
+
+import UIKit
+
+class sujectCollectionViewCell: UICollectionViewCell {
+    
+}
